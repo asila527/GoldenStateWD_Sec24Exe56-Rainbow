08b6542d53f66fbe407a79d155daa5511bc502b6
Ellie S <asila527@gmail.com>
2020-10-26 11:14:55 -0700

    add files

---
+++ RainbowTest.js
@@ -0,0 +1,12 @@
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+colors[0];
+
+//YOU CODE GOES HERE:
+let spans = document.querySelectorAll('span');
+
+for (let i = 0; i < spans.length; i++) {
+        spans[i].style.color = colors[i];
+}
+
+
+
